c76690d785fed1b91ace726b47a745c72100c5e0
KIO2gamer <77529753+KIO2gamer@users.noreply.github.com>
2026-08-07 23:05:59 +0530

    Update q5.c

---
+++ week 1/q5.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+
+int isPrime(int num) {
+    if (num <= 1) {
+        return 0;
+    }
+    
+    for (int i = 2; i * i <= num; i++) {
+        if (num % i == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
+
+int main() {
+    int a, b, temp;
+
+    printf("Enter two numbers (a and b): ");
+    if (scanf("%d %d", &a, &b) != 2) {
+        printf("Invalid input.\n");
+        return 1;
+    }
+
+    if (a > b) {
+        temp = a;
+        a = b;
+        b = temp;
+    }
+
+    printf("Prime numbers between %d and %d are:\n", a, b);
+
+    int found = 0;
+    for (int i = a; i <= b; i++) {
+        if (isPrime(i)) {
+            printf("%d ", i);
+            found = 1;
+        }
+    }
+
+    if (!found) {
+        printf("None");
+    }
+    printf("\n");
+
+    return 0;
+}
